64661bafb1f26d741dd8e8c703858f6696932da5
ibenusinaa <ibenu.sina@gmail.com>
2021-07-11 15:44:20 +0700

    try this one

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
-const NotesValidator = require('./validator/notes')
+const NotesValidator = require('./validator/notes');
  
 const init = async () => {
   const notesService = new NotesService();
